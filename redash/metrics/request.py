2084e7e7f4bdbb56440e626614359ed576c37a12
tahege <tom.hege@consensys.net>
2018-06-21 20:08:01 -0400

    This seems to be the more correct way to handle this

--- redash/metrics/request.py
+++ redash/metrics/request.py
@@ -2,36 +2,18 @@ import logging
 import time
 import requests
 
+from jose import jwt
 from collections import namedtuple
 
 from flask import g, request
-
 from redash import statsd_client, settings
+from redash.authentication import get_jwt_public_key
 
 metrics_logger = logging.getLogger("metrics")
 
 
 def record_request_start_time():
     g.start_time = time.time()
-
-
-def ensure_jwt_freshness():
-    jwttoken = request.cookies.get('jwt', None)
-
-    if jwttoken not None:
-        iat = jwttoken['iat']
-        exp = jwttoken['exp']
-        now = time.time()
-
-        if iat + 1200 < now <= exp:
-            resp = requests.post(settings.REMOTE_JWT_REFRESH_PROVIDER, headers={ 'Authorization' : 'Bearer ' + jwttoken })
-            if resp.status_code > 300 and resp.data.get('jwt', None) not None:
-                request.cookies['jwt'] = resp.data['jwt']
-            elif resp.status_code == 401:
-                return redirect(settings.REMOTE_JWT_EXPIRED_ENDPOINT + '?orig_url=/analytics')
-        elif now > exp:
-            return redirect(settings.REMOTE_JWT_EXPIRED_ENDPOINT + '?orig_url=/analytics')
-
 
 
 def calculate_metrics(response):
@@ -70,6 +52,3 @@ def provision_app(app):
     app.before_request(record_request_start_time)
     app.after_request(calculate_metrics)
     app.teardown_request(calculate_metrics_on_exception)
-
-    if settings.REMOTE_JWT_LOGIN_ENABLED:
-        app.before_request(ensure_jwt_freshness)
