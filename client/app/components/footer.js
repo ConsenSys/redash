4beaf52802a1b03749b84ad8bf0c6f6846673968
tahege <tom.hege@consensys.net>
2018-07-20 15:38:00 -0400

    Adding some of our own personal branding

--- client/app/components/footer.js
+++ client/app/components/footer.js
@@ -3,17 +3,7 @@ import { react2angular } from 'react2angular';
 
 class Footer extends React.Component {
   render() {
-    const version = this.props.clientConfig.version;
-    const newVersionAvailable = this.props.clientConfig.newVersionAvailable && this.props.currentUser.isAdmin;
-    return (
-      <div id="footer">
-        <a href="http://redash.io">Redash</a> {version}{ <small><a href="https://version.redash.io/">(New Redash version available)</a></small> ? newVersionAvailable : ''}
-        &#8226;
-        <a href="https://redash.io/help/">Documentation</a>
-        &#8226;
-        <a href="http://github.com/getredash/redash">Contribute</a>
-      </div>
-    );
+    return null;
   }
 }
 
